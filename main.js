7f06933f88f619fbd54a7afeb992d711a665ae6c
irok <t.irokawa@gmail.com>
2014-06-30 15:55:40 +0900

    現在のURLがcanonical URLと同じ場合は処理しないよう修正

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 		return;
 
 	var link = document.querySelector('link[rel="canonical"]');
-	if (link !== null && link.href.indexOf(location.origin + '/') === 0) {
+	if (link !== null && link.href.indexOf(location.origin + '/') === 0 && link.href !== location.href) {
 		var data = {
 			originalUrl: location.href
 		};
