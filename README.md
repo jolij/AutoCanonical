ad25aa175dbfdf5a16983a20d1946e99694fc29a
irok <t.irokawa@gmail.com>
2014-06-30 14:16:13 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# CanonicaURL
+
+Rewrite the address bar to the canonical URL if the page has a canonical URL.
+
---
+++ main.js
@@ -0,0 +1,12 @@
+(function() {
+	if (window.self !== window.top)
+		return;
+
+	var link = document.querySelector('link[rel="canonical"]');
+	if (link !== null && link.href.indexOf(location.origin) === 0) {
+		var data = {
+			originalUrl: location.href
+		};
+		history.replaceState(data, '', link.href);
+	}
+})();
